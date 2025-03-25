202190acb2400ea8464791e68dfd4196f2448046
Dasky <32983009+daskygit@users.noreply.github.com>
2025-03-25 13:02:41 +0000

    add refresh pcb vial firmware

---
+++ keyboards/mechboards/crkbd/pro/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xCE, 0xD1, 0x3E, 0x77, 0x41, 0x24, 0xA3, 0x87}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
---
+++ keyboards/mechboards/sofle/pro/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xCF, 0x28, 0x94, 0xAF, 0x53, 0x24, 0xDD, 0x89}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
