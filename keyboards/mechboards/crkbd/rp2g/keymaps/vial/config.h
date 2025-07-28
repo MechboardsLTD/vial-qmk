3e00bb9d6319de5224a4b1c8c36cbf54f8056ccd
Dasky <32983009+daskygit@users.noreply.github.com>
2025-07-28 15:56:11 +0100

    add max

---
+++ keyboards/mechboards/crkbd/rp2g/keymaps/vial/config.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x8C, 0xF2, 0xC2, 0x79, 0xEA, 0xA7, 0xEC, 0x28}
+
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+
+#define VIA_CUSTOM_LIGHTING_ENABLE
---
+++ keyboards/mechboards/crkbd/rp2g/keymaps/vial/rules.mk
@@ -0,0 +1,4 @@
+VIA_ENABLE      = yes    # Enable VIA
+LTO_ENABLE      = yes
+RAW_ENABLE      = yes
+VIAL_ENABLE		= yes
