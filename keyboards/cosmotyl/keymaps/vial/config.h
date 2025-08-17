d0e588b32a773ae71ba5b6d712f00ec92b4eb2b6
Dasky <32983009+daskygit@users.noreply.github.com>
2025-08-17 22:43:28 +0100

    add 5x6_5

---
+++ keyboards/cosmotyl/keymaps/vial/config.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xD6, 0xBD, 0xA6, 0x8D, 0xC1, 0x84, 0x60, 0x3D}
+
+// Vial unlock
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+
+// Vial features
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+#define VIAL_KEY_OVERRIDE_ENTRIES 8
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define VIALRGB_NO_DIRECT
