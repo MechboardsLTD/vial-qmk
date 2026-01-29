c87b366563daa2b9f00ace3e8e1de7781ede9aaa
Dasky <32983009+daskygit@users.noreply.github.com>
2026-01-29 13:17:09 +0000

    add micropad

---
+++ keyboards/handwired/mechboards_micropad/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x3A, 0xDB, 0x55, 0xD9, 0x01, 0xB4, 0x9C, 0x29}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
