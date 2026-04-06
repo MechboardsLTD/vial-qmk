c62f531df9a59cd9a7c188fadfe1a5f86e43dd7a
Dasky <32983009+daskygit@users.noreply.github.com>
2026-04-06 14:12:59 +0100

    sofle vial

---
+++ keyboards/mechboards/sofle/r2g/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x95, 0xEB, 0xF9, 0x9C, 0x56, 0x05, 0x46, 0x82}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
--- keyboards/mechboards/sofle/r2g/r2g.c
+++ keyboards/mechboards/sofle/r2g/r2g.c
@@ -9,6 +9,8 @@ void keyboard_post_init_kb(void) {
     keyboard_post_init_user();
 }
 
+#define MATRIX_ROWS_PER_HAND (MATRIX_ROWS / 2)
+
 const pin_t row_pins_left[MATRIX_ROWS_PER_HAND]  = MATRIX_ROW_PINS;
 const pin_t row_pins_right[MATRIX_ROWS_PER_HAND] = MATRIX_ROW_PINS_RIGHT;
 const pin_t col_pins_left[MATRIX_COLS]  = MATRIX_COL_PINS;
