04dc98d21627f16dc8f0f76d15cdfdfea34d6b89
Dasky <32983009+daskygit@users.noreply.github.com>
2026-05-02 16:42:32 +0100

    add vial keymap for crkbd

---
+++ keyboards/mechboards/crkbd/r2g/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x28, 0xAA, 0xC5, 0xDC, 0x73, 0x7D, 0x11, 0x66}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
